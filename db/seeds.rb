59966d7b4dca75bff8bbab1367c581d996dd3283
DaseulChun <nandaseul@gmail.com>
2019-09-23 11:16:09 -0700

    basic CRUD for idea

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Idea.destroy_all
+
+NUM_IDEAS = 30
+
+NUM_IDEAS.times do
+  created_at = Faker::Date.backward(days: 365 * 5)
+  Idea.create(
+    title: Faker::ChuckNorris.fact,
+    description: Faker::Hacker.say_something_smart,
+    created_at: created_at,
+    updated_at: created_at,
+  )
+end
+
+ideas = Idea.all
+
+puts Cowsay.say("Generated #{ideas.count} ideas", :frogs)
